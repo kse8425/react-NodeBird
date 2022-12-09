506f6696396af8e7972d1b37b7e9ebd80c7f7ed3
Kim SungEun <kse8425z@gmail.com>
2022-12-09 20:53:21 +0900

    chapter 2 완료!

--- front/components/PostCard.js
+++ front/components/PostCard.js
@@ -5,6 +5,7 @@ import { RetweetOutlined, HeartOutlined, HeartTwoTone, MessageOutlined, Ellipsis
 import { useSelector } from 'react-redux';
 import PostImages from './PostImages';
 import CommentForm from './CommentForm';
+import PostCardContent from './PostCardContent';
 
 const PostCard = ({ post }) => {
   const [liked, setLiked] = useState(false);
@@ -50,7 +51,7 @@ const PostCard = ({ post }) => {
         <Card.Meta
           avatar={<Avatar>{post.User.nickname[0]}</Avatar>}
           title={post.User.nickname}
-          description={post.content}
+          description={<PostCardContent postData={post.content} />}
         />
       </Card>
       {commentFormOpened && (
--- front/components/PostImages.js
+++ front/components/PostImages.js
@@ -1,8 +1,68 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import PropTypes from 'prop-types';
+import { PlusOutlined } from '@ant-design/icons';
+import ImagesZoom from './ImagesZoom';
 
 const PostImages = ({ images }) => {
-  return <div>구현중...</div>;
+  const [showImagesZoom, setShowImagesZoom] = useState(false);
+  const onZoom = useCallback(() => {
+    setShowImagesZoom(true);
+  }, []);
+  const onClose = useCallback(() => {
+    setShowImagesZoom(false);
+  }, []);
+  if (images.length === 1) {
+    return (
+      <>
+        <img role="presentation" src={images[0].src} alt={images[0].src} onClick={onZoom} />
+        {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
+      </>
+    );
+  } else if (images.length === 2) {
+    return (
+      <>
+        <img
+          role="presentation"
+          style={{ display: 'inline-block', width: '50%' }}
+          src={images[0].src}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
+        <img
+          role="presentation"
+          style={{ display: 'inline-block', width: '50%' }}
+          src={images[1].src}
+          alt={images[1].src}
+          onClick={onZoom}
+        />
+        {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
+      </>
+    );
+  } else {
+    return (
+      <>
+        <div>
+          <img
+            role="presentation"
+            style={{ display: 'inline-block', width: '50%' }}
+            src={images[0].src}
+            alt={images[0].src}
+            onClick={onZoom}
+          />
+          <div
+            role="presentation"
+            style={{ display: 'inline-block', width: '50%', textalign: 'center', verticalalign: 'middle' }}
+            onClick={onZoom}
+          >
+            <PlusOutlined />
+            <br />
+            {images.length - 1} 개의 사진 더보기
+          </div>
+        </div>
+        {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
+      </>
+    );
+  }
 };
 
 PostImages.propTypes = {
