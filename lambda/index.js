009ac39c8cd3c04fbc7e9aa9a8555131c88ce82c
Kim SungEun <kse8425z@gmail.com>
2023-01-23 16:15:12 +0900

    lambda index fix

--- lambda/index.js
+++ lambda/index.js
@@ -5,7 +5,7 @@ const s3 = new AWS.S3();
 
 exports.handler = async (event, context, callback) => {
   const Bucket = event.Records[0].s3.bucket.name;
-  const Key = decodeURIComponent(event.Records[0].s3.object.key());
+  const Key = decodeURIComponent(event.Records[0].s3.object.key);
   console.log(Bucket, Key);
   const filename = Key.split('/')[Key.split('/').length - 1];
   const ext = Key.split('.')[Key.split('.').length - 1].toLowerCase();
