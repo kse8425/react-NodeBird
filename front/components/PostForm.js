846c134e094503a1ac6923f130682b8c028d6fce
Kim SungEun <kse8425z@gmail.com>
2023-01-23 16:47:25 +0900

    수정

--- front/components/PostForm.js
+++ front/components/PostForm.js
@@ -56,21 +56,18 @@ const PostForm = () => {
   }, []);
   return (
     <Form style={{ margin: '10px 0 20px' }} encType="multipart/form-data" onFinish={onSubmit}>
-      <Input.TextArea
-        value={text}
-        onChange={onChangeText}
-        maxLength={140}
-        placeholder="어떤 신기한 일이 있었나요?"
-      />
+      <Input.TextArea value={text} onChange={onChangeText} maxLength={140} placeholder="내용을 입력하세요." />
       <div>
         <input type="file" name="image" multiple hidden ref={imageInput} onChange={onChangeImages} />
         <Button onClick={onClickImageUpload}>이미지 업로드</Button>
-        <Button type="primary" style={{ float: 'right' }} htmlType="submit">짹짹</Button>
+        <Button type="primary" style={{ float: 'right' }} htmlType="submit">
+          짹짹
+        </Button>
       </div>
       <div>
         {imagePaths.map((v, i) => (
           <div key={v} style={{ display: 'inline-block' }}>
-            <img src={v.replace(/\/thumb\//, '/original/')} style={{ width: '200px' }} alt={v} />
+            <img src={v.replace(/\/thumb\//, '/original/')} alt={v} style={{ width: '200px' }} />
             <div>
               <Button onClick={onRemoveImage(i)}>제거</Button>
             </div>
